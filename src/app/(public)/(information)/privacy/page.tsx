100c05ff68898ad458531ca2aa754518096bf4f8
lanstheprodigy <alan.040219.gam@gmail.com>
2025-09-29 05:45:36 +0700

    ketinggalan terms cons nya

---
+++ src/app/(public)/(information)/privacy/page.tsx
@@ -0,0 +1,11 @@
+import { getAppData } from "@/actions/app-data";
+import RichTextStyles from "@/components/editor/rich-text-styles";
+
+export default async function PrivacyPage() {
+  const appData = await getAppData();
+  return (
+    <section className={"mx-auto w-full max-w-4xl px-4 sm:px-6 lg:px-8 py-26"}>
+      <RichTextStyles content={appData.pagePrivacy} />
+    </section>
+  );
+}
