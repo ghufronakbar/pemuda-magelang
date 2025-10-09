bdaa8bfe3534a430acb0ee07d29d59eb3eb37f1a
lanstheprodigy <alan.040219.gam@gmail.com>
2025-10-09 07:14:51 +0700

    update maintenance

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -1,31 +1,30 @@
-"use client";
-
 import { CdnImage } from "@/components/custom/cdn-image";
 import { LOGO } from "@/constants";
 
+export const metadata = {
+  robots: { index: false, follow: false },
+};
+
 export default function Maintenance() {
   return (
-    <>
-      <meta name="robots" content="noindex,nofollow" />
-      <div className="flex min-h-[calc(100vh-100px)] flex-col items-center justify-center">
-        <div className="text-center flex flex-col items-center gap-4">
-          <CdnImage
-            uniqueKey={LOGO}
-            alt="Pemeliharaan"
-            className="h-40 w-40 rounded"
-            width={400}
-            height={400}
-          />
-          <h1 className="text-4xl font-bold">Pemeliharaan</h1>
-          <p className="text-lg">
-            Kami sedang melakukan pemeliharaan pada aplikasi kami. Silakan coba
-            lagi nanti.
-          </p>
-          <p className="text-sm text-muted-foreground">
-            Kami akan segera kembali. Silakan coba lagi nanti. Terima kasih.
-          </p>
-        </div>
+    <div className="flex min-h-[calc(100vh-100px)] flex-col items-center justify-center">
+      <div className="text-center flex flex-col items-center gap-4 px-8">
+        <CdnImage
+          uniqueKey={LOGO}
+          alt="Pemeliharaan"
+          className="h-40 w-40 rounded"
+          width={400}
+          height={400}
+        />
+        <h1 className="text-4xl font-bold">Pemeliharaan</h1>
+        <p className="text-lg">
+          Kami sedang melakukan pemeliharaan pada aplikasi kami. Silakan coba
+          lagi nanti.
+        </p>
+        <p className="text-sm text-muted-foreground">
+          Kami akan segera kembali. Silakan coba lagi nanti. Terima kasih.
+        </p>
       </div>
-    </>
+    </div>
   );
 }
